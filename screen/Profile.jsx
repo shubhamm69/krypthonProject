72473d9a2aecfcbf5cf16384f26370852fba9fc5
Shubham Singh <shubham212a@gmail.com>
2023-03-26 17:06:57 +0530

    push try

--- screen/Profile.jsx
+++ screen/Profile.jsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-500">Profile</Text>
+      <Text className="text-red-500">Profile overview</Text>
       <StatusBar style="auto"/>
     </View>
   );
